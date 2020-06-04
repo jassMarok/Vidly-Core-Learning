ffeb6698cf324035dad75951388dc75ad253e43a
Jaspal Marok <mailjaspalmarok@gmail.com>
2020-06-03 22:46:45 -0400

    Add domain models to dbContext

--- Vidly/VidlyDbContext.cs
+++ Vidly/VidlyDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Vidly.Models;
 
 namespace Vidly
 {
@@ -12,6 +13,9 @@ namespace Vidly
         {
             
         }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
